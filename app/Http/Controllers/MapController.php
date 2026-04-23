3a029c1be77f2314491477f4f7e27fed97a4ad8a
111Roainialfiona <148738023+Roainialfiona@users.noreply.github.com>
2026-04-23 15:34:19 +0700

    Update MapController.php

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -233,7 +233,8 @@ class MapController extends Controller
         $sheet = $request->sheet;
 
         $client = new \Google_Client();
-        $client->setAuthConfig(storage_path('app/credentials.json'));
+        $credentials = json_decode(env('GOOGLE_CREDENTIALS'), true);
+        $client->setAuthConfig($credentials);
         $client->addScope(\Google_Service_Sheets::SPREADSHEETS);
 
         $service = new \Google_Service_Sheets($client);
