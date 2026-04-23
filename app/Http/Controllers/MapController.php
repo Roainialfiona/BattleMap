62b34002c02c2d8cdb904065d029968c622ffcea
unknown <roainialfiona20@gmail.com>
2026-04-23 14:05:35 +0700

    fix credentials from env

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -17,7 +17,8 @@ class MapController extends Controller
 
             $client = new \Google_Client();
             $client->setApplicationName('Battle Map');
-            $client->setAuthConfig(storage_path('app/credentials.json'));
+            $credentials = json_decode(env('GOOGLE_CREDENTIALS'), true);
+            $client->setAuthConfig($credentials);
             $client->addScope(\Google_Service_Sheets::SPREADSHEETS);
 
             $service = new \Google_Service_Sheets($client);
